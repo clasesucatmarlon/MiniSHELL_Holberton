0aae9a9469ab4b4b71a683965783efb624e49923
Andres Callejas G <andres.callejasg@gmail.com>
2020-04-03 22:09:32 +0000

    real fwe

--- fwe.c
+++ fwe.c
@@ -14,13 +14,13 @@ int main()
 {
     int i, status;
     pid_t child_pid;
-    char *av[] = {"/bin/ls", "-l", "/tmp", NULL};
+    char *av[] = {"/bin/ls", NULL};
 
     for (i = 0; i < 5; i++)
     {
         child_pid = fork();
         if (child_pid == 0)
-        {
+        {   
             if (execve(av[0], av, NULL) == -1)
             {
                 perror("Error:");
@@ -31,7 +31,8 @@ int main()
             wait(&status);
             printf("wait %d finished\n", i);
 
-        }        
+        }
+
     }
     return (0);
 }
