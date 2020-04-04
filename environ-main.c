3daaaa8934e95fa60faa1311ff9a1723eda41590
Andres Callejas G <andres.callejasg@gmail.com>
2020-04-04 04:10:05 +0000

    env

---
+++ environ-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the environment
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+    unsigned int i;
+    extern char **environ;
+
+    i = 0;
+
+    while (environ[i] != NULL)
+    {
+        printf("%s\n", environ[i]);
+        i++;
+    }
+    return (0);
+}
